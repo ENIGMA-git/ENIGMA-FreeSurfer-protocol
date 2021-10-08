f05f50afe99926af783ffd8c14ac917fb661dd2c
sthomopo <thomopoulos.s@gmail.com>
2021-10-07 17:10:16 -0700

    Create 01_extract_subcortical_FreeSurfer_measures_v2021.sh

---
+++ subcortical/01_extract_subcortical_FreeSurfer_measures_v2021.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#$ -S /bin/bash
+
+# Where your FreeSurfer outputs are saved
+fs_dir=/enigma/Parent_Folder/FreeSurfer/outputs/
+# Where your csv will be saved
+dir=/enigma/Parent_Folder/FreeSurfer/measures/
+
+mkdir ${fs_dir}
+
+cd ${fs_dir} 
+echo "SubjID,LLatVent,RLatVent,Lthal,Rthal,Lcaud,Rcaud,Lput,Rput,Lpal,Rpal,Lhippo,Rhippo,Lamyg,Ramyg,Laccumb,Raccumb,ICV" > ${dir}LandRvolumes.csv
+
+for subj_id in `ls -d Subj*`
+do 
+printf "%s,"  "${subj_id}" >> ${dir}LandRvolumes.csv
+for x in Left-Lateral-Ventricle Right-Lateral-Ventricle Left-Thalamus-Proper Right-Thalamus-Proper  Left-Caudate Right-Caudate Left-Putamen Right-Putamen Left-Pallidum Right-Pallidum Left-Hippocampus Right-Hippocampus Left-Amygdala Right-Amygdala Left-Accumbens-area Right-Accumbens-area; do
+printf "%g," `grep  ${x} ${subj_id}/stats/aseg.stats | awk '{print $4}'` >> ${dir}LandRvolumes.csv
+done
+printf "%f" `cat ${subj_id}/stats/aseg.stats | grep IntraCranialVol | awk -F, '{print $4}'` >> ${dir}LandRvolumes.csv
+echo "" >> ${dir}LandRvolumes.csv
+done
+
+# NB: if you are using FreeSurfer v7 and above, replace Left-Thalamus-Proper and Right-Thalamus-Proper with Left-Thalamus and Right-Thalamus, respectively.
