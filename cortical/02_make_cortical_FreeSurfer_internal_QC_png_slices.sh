edd4eba6d3ff731bbe0c896f5ca6c827d9fb0e90
sthomopo <thomopoulos.s@gmail.com>
2021-11-02 14:49:12 -0700

    Update 02_make_cortical_FreeSurfer_internal_QC_png_slices.sh

--- cortical/02_make_cortical_FreeSurfer_internal_QC_png_slices.sh
+++ cortical/02_make_cortical_FreeSurfer_internal_QC_png_slices.sh
@@ -6,9 +6,9 @@ for subj in subject1 subject2 subject3;
 do
 echo $subj
 
-# freesurfer parent directory
+# Input directory, i.e. Freeurfer outputs
 fs_dir=/enigma/Parent_Folder/FreeSurfer/outputs/
-# QC output directory
+# QC output directory, i.e. where your PNGs will be saved
 qc_dir=/enigma/Parent_Folder/FreeSurfer/QC_cortical_internal/
 
 mkdir ${qc_dir}
@@ -17,15 +17,16 @@ b=${subj} #subject folder
 imageF=${fs_dir}/${b}/mri/orig_nu.mgz
 overlay=${fs_dir}/${b}/mri/aparc+aseg.mgz
 
-#where your ENIGMA_QC folder is copied
-cd /enigma/Parent_Folder/scripts/ENIGMA_QC/
+# Define where your script and matlab functions are saved from the zip file 
+scripts=/enigma/Parent_Folder/scripts/ENIGMA_QC/
+cd ${scripts}
 dirs
 
 if [ -f ${imageF} ]
 then
 
 matlabcall="func_make_corticalpngs_ENIGMA_QC('${qc_dir}','${b}','${imageF}','${overlay}')"
-# Point to where your matlab is saved
+# Define where matlab is located on your system
 /usr/local/matlab/bin/matlab -nodisplay -nosplash -singleCompThread -r "${matlabcall};quit"
 echo 'Done with subject: ' ${subj}
 
