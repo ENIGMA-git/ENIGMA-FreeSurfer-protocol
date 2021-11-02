852a5487041232d22d89d1d982b70f6e86e9e1b4
sthomopo <thomopoulos.s@gmail.com>
2021-11-02 14:49:21 -0700

    Update 03_make_cortical_FreeSurfer_external_QC_png_v2021.sh

--- cortical/03_make_cortical_FreeSurfer_external_QC_png_v2021.sh
+++ cortical/03_make_cortical_FreeSurfer_external_QC_png_v2021.sh
@@ -6,18 +6,18 @@ printf "Running External FreeSurfer \n"
 printf "written by Iyad Ba Gari (IGC USC) - Apr 2021\n"
 printf "+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\n"
 
-# Input directory, ie Freeurfer outputs
+# Input directory, i.e. Freeurfer outputs
 fs_dir=/enigma/Parent_Folder/Freesurfer/outputs/
-# Output directory, ie where your PNGs will be saved
-qc_dir=/enigma/Parent_Folder/Freesurfer/QC_cortical_external
-mkdir ${qc_dir}
+# QC output directory, i.e. where your PNGs will be saved
+qc_dir=/enigma/Parent_Folder/FreeSurfer/QC_cortical_external
+mkdir -p ${qc_dir}
 
-# Define where your script and matlab functions are saved from the ENIGMA_QC_cortical_updApril2021.zip zip file 
+# Define where your script and matlab functions are saved from the zip file 
 scripts=/enigma/Parent_Folder/scripts/ENIGMA_QC/
 cd ${scripts}
 
-for subject in subject1 subject2 subject3;
+for subj in subject1 subject2 subject3;
 do
-# Define where matlab is located on your server
-/usr/local/matlab/bin/matlab -nodisplay -batch "FS_external_QC('${fs_dir}', '${qc_dir}', '${subject}')"
+# Define where matlab is located on your system
+/usr/local/matlab/bin/matlab -nodisplay -batch "FS_external_QC('${fs_dir}', '${qc_dir}', '${subj}')"
 done
