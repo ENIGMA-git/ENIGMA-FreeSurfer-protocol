4c6a3bd7517c94c786426ae7ef510ea53197c795
Iyad Ba Gari <bagari@usc.edu>
2026-03-02 17:08:46 -0800

    Support FreeSurfer 8

--- enigma_fs_wrapper_script.sh
+++ enigma_fs_wrapper_script.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# 06/03/2022
 # Wrapper written by Iyad Ba Gari and Sophia Thomopoulos (enigma@ini.usc.edu)
 # This script is to be run on your subjects after you have run FreeSurfer on your T1 data
 
@@ -12,12 +11,12 @@ Usage:
 `basename $0` --subjects <subject.txt> --fsdir </freesufer/path> --outdir <output/directory> --script <ENIGMA/scripts/path> --matlab <MATLAB/path> --step_N 1 <other options>
 
 Mandatory arguments:
-	--subjects	Provide a list of subjects stored in a text file
-	--fsdir		FreeSurfer output path; absolute path
-	--outdir	Output directory
-	--script	Absolute path to ENIGMA dependency scripts
-	--matlab	Matlab path
-	--fs7		Freesurfer v7 and above (e.g. --fs true or false)
+	--subjects		Provide a list of subjects stored in a text file
+	--fsdir			FreeSurfer output path; absolute path
+	--outdir		Output directory
+	--script		Absolute path to ENIGMA dependency scripts
+	--matlab		Matlab path
+	--fs_version	Freesurfer version (choose from 5, 6, 7 ,or 8)
 
 N.B. --step_(1:5) choose at least one of these steps 
 
@@ -70,7 +69,7 @@ case $key in
         shift 
         shift 
         ;;
-	--fs7)
+	--fs_version)
         fs_version=${2}
         shift 
         shift 
@@ -121,9 +120,21 @@ if [[ "${step_val_1}" = true ]]; then
 	printf "+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\n"
 
 	measures_dir=${out_dir}/measures && mkdir -p ${measures_dir}
-	echo "SubjID,LLatVent,RLatVent,Lthal,Rthal,Lcaud,Rcaud,Lput,Rput,Lpal,Rpal,Lhippo,Rhippo,Lamyg,Ramyg,Laccumb,Raccumb,ICV" > ${measures_dir}/LandRvolumes.csv
-	if [[ "${fs_version}" = true ]]
-	then
+	echo "SubjID,LLatVent,RLatVent,Lthal,Rthal,Lcaud,Rcaud,Lput,Rput,Lpal,Rpal,Lhippo,Rhippo,Lamyg,Ramyg,Laccumb,Raccumb,ICV,Freesurfer_Version" > ${measures_dir}/LandRvolumes.csv
+
+	# setting default to Freesurfer v7 if version not specified:
+	case "${fs_version}" in 
+		5|6|7|8)
+			echo "running for Freesurfer version ${fs_version}"
+		;;
+		*)
+			echo "version not found, setting to default Freesurfer 7"
+			fs_version=7
+		;;
+	esac
+
+
+	if [[ "${fs_version}" -ge 7 ]]; then
 		for subj_id in `cat $subjectIDs`; do
 			printf "%s,"  "${subj_id}" >> ${measures_dir}/LandRvolumes.csv
 			# using FreeSurfer v7 and above
@@ -131,6 +142,8 @@ if [[ "${step_val_1}" = true ]]; then
 				printf "%g," `grep  ${x} ${fs_dir}/${subj_id}/stats/aseg.stats | awk '{print $4}'` >> ${measures_dir}/LandRvolumes.csv
 			done
 			printf "%f" `cat ${fs_dir}/${subj_id}/stats/aseg.stats | grep IntraCranialVol | awk -F, '{print $4}'` >> ${measures_dir}/LandRvolumes.csv
+
+			printf ",%s" $(grep -oE "[0-100]+\.[0-100]+\.[0-100]+" ${fs_dir}/${subj_id}/scripts/build-stamp.txt) >> ${measures_dir}/LandRvolumes.csv
 			echo "" >> ${measures_dir}/LandRvolumes.csv
 		done
 	else
@@ -140,6 +153,8 @@ if [[ "${step_val_1}" = true ]]; then
 				printf "%g," `grep  ${x} ${fs_dir}/${subj_id}/stats/aseg.stats | awk '{print $4}'` >> ${measures_dir}/LandRvolumes.csv
 			done
 			printf "%f" `cat ${fs_dir}/${subj_id}/stats/aseg.stats | grep IntraCranialVol | awk -F, '{print $4}'` >> ${measures_dir}/LandRvolumes.csv
+
+			printf ",%s" $(grep -oE "[0-100]+\.[0-100]+\.[0-100]+" ${fs_dir}/${subj_id}/scripts/build-stamp.txt) >> ${measures_dir}/LandRvolumes.csv
 			echo "" >> ${measures_dir}/LandRvolumes.csv
 		done
 	fi
@@ -153,8 +168,8 @@ if [[ "${step_val_2}" = true ]]; then
 
 	measures_dir=${out_dir}/measures && mkdir -p ${measures_dir}
 
-	echo 'SubjID,L_bankssts_thickavg,L_caudalanteriorcingulate_thickavg,L_caudalmiddlefrontal_thickavg,L_cuneus_thickavg,L_entorhinal_thickavg,L_fusiform_thickavg,L_inferiorparietal_thickavg,L_inferiortemporal_thickavg,L_isthmuscingulate_thickavg,L_lateraloccipital_thickavg,L_lateralorbitofrontal_thickavg,L_lingual_thickavg,L_medialorbitofrontal_thickavg,L_middletemporal_thickavg,L_parahippocampal_thickavg,L_paracentral_thickavg,L_parsopercularis_thickavg,L_parsorbitalis_thickavg,L_parstriangularis_thickavg,L_pericalcarine_thickavg,L_postcentral_thickavg,L_posteriorcingulate_thickavg,L_precentral_thickavg,L_precuneus_thickavg,L_rostralanteriorcingulate_thickavg,L_rostralmiddlefrontal_thickavg,L_superiorfrontal_thickavg,L_superiorparietal_thickavg,L_superiortemporal_thickavg,L_supramarginal_thickavg,L_frontalpole_thickavg,L_temporalpole_thickavg,L_transversetemporal_thickavg,L_insula_thickavg,R_bankssts_thickavg,R_caudalanteriorcingulate_thickavg,R_caudalmiddlefrontal_thickavg,R_cuneus_thickavg,R_entorhinal_thickavg,R_fusiform_thickavg,R_inferiorparietal_thickavg,R_inferiortemporal_thickavg,R_isthmuscingulate_thickavg,R_lateraloccipital_thickavg,R_lateralorbitofrontal_thickavg,R_lingual_thickavg,R_medialorbitofrontal_thickavg,R_middletemporal_thickavg,R_parahippocampal_thickavg,R_paracentral_thickavg,R_parsopercularis_thickavg,R_parsorbitalis_thickavg,R_parstriangularis_thickavg,R_pericalcarine_thickavg,R_postcentral_thickavg,R_posteriorcingulate_thickavg,R_precentral_thickavg,R_precuneus_thickavg,R_rostralanteriorcingulate_thickavg,R_rostralmiddlefrontal_thickavg,R_superiorfrontal_thickavg,R_superiorparietal_thickavg,R_superiortemporal_thickavg,R_supramarginal_thickavg,R_frontalpole_thickavg,R_temporalpole_thickavg,R_transversetemporal_thickavg,R_insula_thickavg,LThickness,RThickness,LSurfArea,RSurfArea,ICV' > ${measures_dir}/CorticalMeasuresENIGMA_ThickAvg.csv
-	echo 'SubjID,L_bankssts_surfavg,L_caudalanteriorcingulate_surfavg,L_caudalmiddlefrontal_surfavg,L_cuneus_surfavg,L_entorhinal_surfavg,L_fusiform_surfavg,L_inferiorparietal_surfavg,L_inferiortemporal_surfavg,L_isthmuscingulate_surfavg,L_lateraloccipital_surfavg,L_lateralorbitofrontal_surfavg,L_lingual_surfavg,L_medialorbitofrontal_surfavg,L_middletemporal_surfavg,L_parahippocampal_surfavg,L_paracentral_surfavg,L_parsopercularis_surfavg,L_parsorbitalis_surfavg,L_parstriangularis_surfavg,L_pericalcarine_surfavg,L_postcentral_surfavg,L_posteriorcingulate_surfavg,L_precentral_surfavg,L_precuneus_surfavg,L_rostralanteriorcingulate_surfavg,L_rostralmiddlefrontal_surfavg,L_superiorfrontal_surfavg,L_superiorparietal_surfavg,L_superiortemporal_surfavg,L_supramarginal_surfavg,L_frontalpole_surfavg,L_temporalpole_surfavg,L_transversetemporal_surfavg,L_insula_surfavg,R_bankssts_surfavg,R_caudalanteriorcingulate_surfavg,R_caudalmiddlefrontal_surfavg,R_cuneus_surfavg,R_entorhinal_surfavg,R_fusiform_surfavg,R_inferiorparietal_surfavg,R_inferiortemporal_surfavg,R_isthmuscingulate_surfavg,R_lateraloccipital_surfavg,R_lateralorbitofrontal_surfavg,R_lingual_surfavg,R_medialorbitofrontal_surfavg,R_middletemporal_surfavg,R_parahippocampal_surfavg,R_paracentral_surfavg,R_parsopercularis_surfavg,R_parsorbitalis_surfavg,R_parstriangularis_surfavg,R_pericalcarine_surfavg,R_postcentral_surfavg,R_posteriorcingulate_surfavg,R_precentral_surfavg,R_precuneus_surfavg,R_rostralanteriorcingulate_surfavg,R_rostralmiddlefrontal_surfavg,R_superiorfrontal_surfavg,R_superiorparietal_surfavg,R_superiortemporal_surfavg,R_supramarginal_surfavg,R_frontalpole_surfavg,R_temporalpole_surfavg,R_transversetemporal_surfavg,R_insula_surfavg,LThickness,RThickness,LSurfArea,RSurfArea,ICV' > ${measures_dir}/CorticalMeasuresENIGMA_SurfAvg.csv
+	echo 'SubjID,L_bankssts_thickavg,L_caudalanteriorcingulate_thickavg,L_caudalmiddlefrontal_thickavg,L_cuneus_thickavg,L_entorhinal_thickavg,L_fusiform_thickavg,L_inferiorparietal_thickavg,L_inferiortemporal_thickavg,L_isthmuscingulate_thickavg,L_lateraloccipital_thickavg,L_lateralorbitofrontal_thickavg,L_lingual_thickavg,L_medialorbitofrontal_thickavg,L_middletemporal_thickavg,L_parahippocampal_thickavg,L_paracentral_thickavg,L_parsopercularis_thickavg,L_parsorbitalis_thickavg,L_parstriangularis_thickavg,L_pericalcarine_thickavg,L_postcentral_thickavg,L_posteriorcingulate_thickavg,L_precentral_thickavg,L_precuneus_thickavg,L_rostralanteriorcingulate_thickavg,L_rostralmiddlefrontal_thickavg,L_superiorfrontal_thickavg,L_superiorparietal_thickavg,L_superiortemporal_thickavg,L_supramarginal_thickavg,L_frontalpole_thickavg,L_temporalpole_thickavg,L_transversetemporal_thickavg,L_insula_thickavg,R_bankssts_thickavg,R_caudalanteriorcingulate_thickavg,R_caudalmiddlefrontal_thickavg,R_cuneus_thickavg,R_entorhinal_thickavg,R_fusiform_thickavg,R_inferiorparietal_thickavg,R_inferiortemporal_thickavg,R_isthmuscingulate_thickavg,R_lateraloccipital_thickavg,R_lateralorbitofrontal_thickavg,R_lingual_thickavg,R_medialorbitofrontal_thickavg,R_middletemporal_thickavg,R_parahippocampal_thickavg,R_paracentral_thickavg,R_parsopercularis_thickavg,R_parsorbitalis_thickavg,R_parstriangularis_thickavg,R_pericalcarine_thickavg,R_postcentral_thickavg,R_posteriorcingulate_thickavg,R_precentral_thickavg,R_precuneus_thickavg,R_rostralanteriorcingulate_thickavg,R_rostralmiddlefrontal_thickavg,R_superiorfrontal_thickavg,R_superiorparietal_thickavg,R_superiortemporal_thickavg,R_supramarginal_thickavg,R_frontalpole_thickavg,R_temporalpole_thickavg,R_transversetemporal_thickavg,R_insula_thickavg,LThickness,RThickness,LSurfArea,RSurfArea,ICV,Freesurfer_Version' > ${measures_dir}/CorticalMeasuresENIGMA_ThickAvg.csv
+	echo 'SubjID,L_bankssts_surfavg,L_caudalanteriorcingulate_surfavg,L_caudalmiddlefrontal_surfavg,L_cuneus_surfavg,L_entorhinal_surfavg,L_fusiform_surfavg,L_inferiorparietal_surfavg,L_inferiortemporal_surfavg,L_isthmuscingulate_surfavg,L_lateraloccipital_surfavg,L_lateralorbitofrontal_surfavg,L_lingual_surfavg,L_medialorbitofrontal_surfavg,L_middletemporal_surfavg,L_parahippocampal_surfavg,L_paracentral_surfavg,L_parsopercularis_surfavg,L_parsorbitalis_surfavg,L_parstriangularis_surfavg,L_pericalcarine_surfavg,L_postcentral_surfavg,L_posteriorcingulate_surfavg,L_precentral_surfavg,L_precuneus_surfavg,L_rostralanteriorcingulate_surfavg,L_rostralmiddlefrontal_surfavg,L_superiorfrontal_surfavg,L_superiorparietal_surfavg,L_superiortemporal_surfavg,L_supramarginal_surfavg,L_frontalpole_surfavg,L_temporalpole_surfavg,L_transversetemporal_surfavg,L_insula_surfavg,R_bankssts_surfavg,R_caudalanteriorcingulate_surfavg,R_caudalmiddlefrontal_surfavg,R_cuneus_surfavg,R_entorhinal_surfavg,R_fusiform_surfavg,R_inferiorparietal_surfavg,R_inferiortemporal_surfavg,R_isthmuscingulate_surfavg,R_lateraloccipital_surfavg,R_lateralorbitofrontal_surfavg,R_lingual_surfavg,R_medialorbitofrontal_surfavg,R_middletemporal_surfavg,R_parahippocampal_surfavg,R_paracentral_surfavg,R_parsopercularis_surfavg,R_parsorbitalis_surfavg,R_parstriangularis_surfavg,R_pericalcarine_surfavg,R_postcentral_surfavg,R_posteriorcingulate_surfavg,R_precentral_surfavg,R_precuneus_surfavg,R_rostralanteriorcingulate_surfavg,R_rostralmiddlefrontal_surfavg,R_superiorfrontal_surfavg,R_superiorparietal_surfavg,R_superiortemporal_surfavg,R_supramarginal_surfavg,R_frontalpole_surfavg,R_temporalpole_surfavg,R_transversetemporal_surfavg,R_insula_surfavg,LThickness,RThickness,LSurfArea,RSurfArea,ICV,Freesurfer_Version' > ${measures_dir}/CorticalMeasuresENIGMA_SurfAvg.csv
 	
 	for subj_id in `cat $subjectIDs`; do
 		printf "%s,"  "${subj_id}" >> ${measures_dir}/CorticalMeasuresENIGMA_ThickAvg.csv
@@ -177,6 +192,10 @@ if [[ "${step_val_2}" = true ]]; then
 		printf "%g," `cat ${fs_dir}/${subj_id}/stats/rh.aparc.stats | grep WhiteSurfArea | awk -F, '{print $4}'` >> ${measures_dir}/CorticalMeasuresENIGMA_SurfAvg.csv
 		printf "%f" `cat ${fs_dir}/${subj_id}/stats/aseg.stats | grep IntraCranialVol | awk -F, '{print $4}'` >> ${measures_dir}/CorticalMeasuresENIGMA_ThickAvg.csv
 		printf "%f" `cat ${fs_dir}/${subj_id}/stats/aseg.stats | grep IntraCranialVol | awk -F, '{print $4}'` >> ${measures_dir}/CorticalMeasuresENIGMA_SurfAvg.csv
+
+		printf ",%s" $(grep -oE "[0-100]+\.[0-100]+\.[0-100]+" ${fs_dir}/${subj_id}/scripts/build-stamp.txt) >> ${measures_dir}/CorticalMeasuresENIGMA_ThickAvg.csv
+		printf ",%s" $(grep -oE "[0-100]+\.[0-100]+\.[0-100]+" ${fs_dir}/${subj_id}/scripts/build-stamp.txt) >> ${measures_dir}/CorticalMeasuresENIGMA_SurfAvg.csv
+
 		echo "" >> ${measures_dir}/CorticalMeasuresENIGMA_ThickAvg.csv
 		echo "" >> ${measures_dir}/CorticalMeasuresENIGMA_SurfAvg.csv
 	done
@@ -192,9 +211,11 @@ if [[ "${step_val_3}" = true ]]; then
 	qc_dir_subcort=${out_dir}/qc/subcortical && mkdir -p ${qc_dir_subcort}
 
 	for subj_id in `cat $subjectIDs`; do
-		matlabcall="addpath(genpath('${scripts_dir}')); func_make_subcorticalFS_ENIGMA_QC('${qc_dir_subcort}','${subj_id}','${fs_dir}/${subj_id}/mri/orig_nu.mgz','${fs_dir}/${subj_id}/mri/aparc+aseg.mgz')"
-		${matlab_dir} -nodisplay -batch "${matlabcall};exit;"
-		echo 'Done with subject: ' ${subj_id}
+		if [[ "${fs_version}" = 8 ]]; then
+			matlabcall="addpath(genpath('${scripts_dir}')); func_make_subcorticalFS_ENIGMA_QC('${qc_dir_subcort}','${subj_id}','${fs_dir}/${subj_id}/mri/nu.mgz','${fs_dir}/${subj_id}/mri/aparc+aseg.mgz')"
+		else
+			matlabcall="addpath(genpath('${scripts_dir}')); func_make_subcorticalFS_ENIGMA_QC('${qc_dir_subcort}','${subj_id}','${fs_dir}/${subj_id}/mri/orig_nu.mgz','${fs_dir}/${subj_id}/mri/aparc+aseg.mgz')"
+		fi
 	done
 
 	#Create QC webpage
@@ -287,7 +308,12 @@ if [[ "${step_val_4}" = true ]]; then
 	qc_dir_cort_int=${out_dir}/qc/cortical_internal && mkdir -p ${qc_dir_cort_int}
 
 	for subj_id in `cat $subjectIDs`; do
-		matlabcall="addpath(genpath('${scripts_dir}')); func_make_corticalpngs_ENIGMA_QC('${qc_dir_cort_int}','${subj_id}','${fs_dir}/${subj_id}/mri/orig_nu.mgz','${fs_dir}/${subj_id}/mri/aparc+aseg.mgz')"
+		if [[ "${fs_version} = 8" ]]; then
+			matlabcall="addpath(genpath('${scripts_dir}')); func_make_corticalpngs_ENIGMA_QC('${qc_dir_cort_int}','${subj_id}','${fs_dir}/${subj_id}/mri/nu.mgz','${fs_dir}/${subj_id}/mri/aparc+aseg.mgz')"
+		else
+			matlabcall="addpath(genpath('${scripts_dir}')); func_make_corticalpngs_ENIGMA_QC('${qc_dir_cort_int}','${subj_id}','${fs_dir}/${subj_id}/mri/orig_nu.mgz','${fs_dir}/${subj_id}/mri/aparc+aseg.mgz')"
+		fi
+
 		${matlab_dir} -nodisplay -batch "${matlabcall};exit;"
 		echo 'Done with subject: ' ${subj_id}
 	done
